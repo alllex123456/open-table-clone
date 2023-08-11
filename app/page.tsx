e648977a776b0a62d4a365ee726d995a8d25f8fa
alllex123456 <alextanase454@gmail.com>
2023-08-11 12:42:00 +0300

    --

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Cuisine, Location, PRICE, PrismaClient, Review } from '@prisma/client';
 export interface RestaurantCardType {
   id: number;
   name: string;
+  main_image: string;
   cuisine: Cuisine;
   location: Location;
   price: PRICE;
